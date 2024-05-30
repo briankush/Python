259e5795c71b2014674fce91478f0c981b5aa24f
Brain kuria <briakush05@gmail.com>
2024-05-30 13:55:36 +0300

    Nesting

--- Dictonaries.py
+++ Dictonaries.py
@@ -12,3 +12,18 @@ for key,value in student.items():
 Home=student.pop('Residence')
 print(f'The student {student["name"]} resides in {Home}. He is {student["age"]}yrs old and currently learning {student['Course']}')    
 del student['sport']
+print(student.items())
+
+#Nesting 
+school={
+    'Name':'Machakos University', 'Location':'Machakos county', 'Schools':[
+        'Engineering and Technology',
+        'Pure and applpied sciences',
+        'Education',
+        'Hospitality and tourism'
+        ]
+    }
+
+school_details=[student, school]
+print (f'{student} \n')
+print(school)
